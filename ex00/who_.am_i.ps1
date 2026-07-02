2fd6196aa737a83c04d55b1dca3e33945d1c9140
tatiana torres <tatianasan.torres@gmail.com>
2026-07-02 22:51:01 +0100

    feat: adicionar resolucao do exercicio 00 - who am i

---
+++ ex00/who_.am_i.ps1
@@ -0,0 +1,26 @@
+$Nome = "Tatiana Torres"
+$Background = "Licenciada em Enfermagem Veterinaria"
+$Expectativa = "Ter uma nova area e diferente para arranjar trabalho"
+$NinjaNivel = "0"
+$Curiosidade = "Os meus hobbies sao ouvir musica, ver series e passear por locais com paisagens bonitas"
+$Specs = "Windows, AMD Ryzen 5 3500U with Radeon Vega Mobile Gfx (2.10 GHz), RAM 8GB (5.88GB utilizavel)"
+
+Write-Host ""
+Write-Host "* Nome:"
+Write-Host $Nome
+Write-Host ""
+Write-Host "* Background:"
+Write-Host $Background
+Write-Host ""
+Write-Host "* Expectativa:"
+Write-Host $Expectativa
+Write-Host ""
+Write-Host "* Nivel Ninja na Consola:"
+Write-Host $NinjaNivel
+Write-Host ""
+Write-Host "* Curiosidade:"
+Write-Host $Curiosidade
+Write-Host ""
+Write-Host "* Especificaçoes do PC:"
+Write-Host $Specs
+Write-Host ""
